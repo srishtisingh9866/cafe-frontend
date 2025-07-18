539c7d2397b8cdd6eaa99888ce1aa9f8d971adfa
srishtisingh9866 <srishtisingh9866@gmail.com>
2025-07-18 14:52:00 +0530

    first commit

---
+++ src/components/Temp.jsx
@@ -0,0 +1,58 @@
+// // import { resolveConfig } from 'vite'
+// import React, { useState } from 'react';
+
+// export default function Temp() {
+//   const [score, setScore] = useState(0);
+
+//   const addScore = () => {
+//     setScore(score + 1);
+//   };
+
+//   const reduceScore = () => {
+//     setScore(score - 1);
+//   };
+
+//   return (
+//     <div>
+//       <h2>Score: {score}</h2>
+//       <p>
+//         <button onClick={addScore}>Increase Score</button>
+//         <button onClick={reduceScore}>Decrease Score</button>
+//       </p>
+//     </div>
+//   );
+// }
+
+// // //rfc + enter
+// // export default function Temp(flag) {
+// //     if(flag)return ( <h1>Flag is True</h1> )
+// //         else return (<h2>Flag is False</h2>)
+// // }
+
+// //ternary operator
+// // export default function Temp(flag) {
+// //     return flag? <h1>Flag is True</h1> : <h2>Flag is False</h2>
+// // }
+
+
+// // export default function Temp(flag) {
+// //     return flag && <h1>Flag is True</h1>
+// // }
+
+// //EVENTS
+
+// // export default function Temp() {
+// //     const handleClick=()=>{
+// //         alert("Hello!!!!");
+// //     }
+// //     const handleSubmit = (name)=>[
+// //         alert(`Hello ! ${name}`)
+// //     ]
+// //     return (<div>
+// //         <button onClick={handleClick}>Click</button>
+// //         <button onClick={()=>handleSubmit("Jhon")}>Submit</button>
+// //     </div>
+// //     );
+// // }
+
+
